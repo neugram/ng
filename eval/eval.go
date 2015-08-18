3ae44fe7b82f4742c987884f9e071439c3cdb702
David Crawshaw <david@zentus.com>
2015-08-17 21:22:32 -0400

    behold the hackiest eval in the world

--- eval/eval.go
+++ eval/eval.go
@@ -10,21 +10,25 @@ import (
 	"numgrad.io/parser"
 )
 
-type Scope struct {
-	Parent *Scope
-	Ident  map[string]interface{}
+type Variable struct {
+	Value interface{}
 }
 
-func Eval(s *Scope, expr parser.Expr) (interface{}, error) {
+type Scope struct {
+	Parent *Scope
+	Ident  map[string]*Variable
+}
+
+func newScope(parent *Scope) *Scope {
+	return &Scope{
+		Parent: parent,
+		Ident:  make(map[string]*Variable),
+	}
+}
+
+// eval reduces expr to a *Variable or literal
+func eval(s *Scope, expr parser.Expr) (interface{}, error) {
 	switch expr := expr.(type) {
-	case *parser.Ident:
-		for s != nil {
-			if v, ok := s.Ident[expr.Name]; ok {
-				return v, nil
-			}
-			s = s.Parent
-		}
-		return nil, fmt.Errorf("eval: undefined identifier: %q", expr.Name)
 	case *parser.BinaryExpr:
 		lhs, err := Eval(s, expr.Left)
 		if err != nil {
@@ -34,13 +38,14 @@ func Eval(s *Scope, expr parser.Expr) (interface{}, error) {
 		if err != nil {
 			return nil, err
 		}
+
 		v, err := binOp(expr.Op, lhs, rhs)
 		if err != nil {
 			return nil, fmt.Errorf("eval: %v evaluating %s", err, expr)
 		}
 		return v, nil
 	case *parser.UnaryExpr:
-		sub, err := Eval(s, expr.Expr)
+		sub, err := eval(s, expr.Expr)
 		if err != nil {
 			return nil, err
 		}
@@ -48,14 +53,149 @@ func Eval(s *Scope, expr parser.Expr) (interface{}, error) {
 		case parser.Not:
 		case parser.Mul: // deref
 		case parser.Ref:
+		case parser.Sub:
+			v, err := evalLitOrVar(sub)
+			if err != nil {
+				return nil, err
+			}
+			return binOp(parser.Sub, big.NewInt(0), v)
 		case parser.LeftParen:
 			return sub, nil
 		}
 	case *parser.CallExpr:
-	case *parser.BasicLiteral:
-		return expr.Value, nil
+		fn, err := eval(s, expr.Func)
+		if err != nil {
+			return nil, err
+		}
+		switch fn := fn.(type) {
+		case *parser.FuncLiteral:
+			fs := newScope(s)
+			for _, stmt := range fn.Body {
+				retVals, returned, err := evalStmt(fs, stmt)
+				if err != nil {
+					return nil, err
+				}
+				if returned {
+					return retVals, nil
+				}
+			}
+			if len(fn.Type.Out) > 0 {
+				return nil, fmt.Errorf("expected return %v", fn.Type.Out)
+			}
+		}
+	case *parser.Ident:
+		for s != nil {
+			if v, ok := s.Ident[expr.Name]; ok {
+				return v, nil
+			}
+			s = s.Parent
+		}
+		// TODO only add to scope if it was declared.
+		v := new(Variable)
+		s.Ident[expr.Name] = v
+		return v, nil
+		//return nil, fmt.Errorf("eval: undefined identifier: %q", expr.Name)
+	case *parser.BasicLiteral, *parser.FuncLiteral, *Variable:
+		return expr, nil
 	}
-	return nil, fmt.Errorf("eval TODO %#+v, %T", expr, expr)
+	return nil, fmt.Errorf("TODO eval(%#+v), %T", expr, expr)
+}
+
+func evalStmt(s *Scope, stmt parser.Stmt) (retVals []interface{}, returned bool, err error) {
+	switch stmt := stmt.(type) {
+	case *parser.AssignStmt:
+		for i, lhs := range stmt.Left {
+			lhs, err = eval(s, lhs)
+			if err != nil {
+				return nil, false, err
+			}
+			lhsvar, ok := lhs.(*Variable)
+			if !ok {
+				return nil, false, fmt.Errorf("assignment to non-variable: %s", lhs)
+			}
+			v, err := eval(s, stmt.Right[i])
+			if err != nil {
+				return nil, false, err
+			}
+			lhsvar.Value = v
+		}
+		return nil, false, nil
+	case *parser.IfStmt:
+		is := newScope(s)
+		if stmt.Init != nil {
+			_, _, err := evalStmt(is, stmt.Init)
+			if err != nil {
+				return nil, false, err
+			}
+		}
+		cond, err := Eval(s, stmt.Cond)
+		if err != nil {
+			return nil, false, err
+		}
+		doBody, ok := cond.(bool)
+		if !ok {
+			return nil, false, fmt.Errorf("if condition not a boolean expression, got: %v", cond)
+		}
+		if doBody {
+			return evalStmt(is, stmt.Body)
+		} else {
+			return evalStmt(is, stmt.Else)
+		}
+	case *parser.ReturnStmt:
+		retVals := make([]interface{}, len(stmt.Exprs))
+		for i, e := range stmt.Exprs {
+			retVals[i], err = eval(s, e)
+			if err != nil {
+				return nil, false, err
+			}
+		}
+		return retVals, true, nil
+	case *parser.BlockStmt:
+		bs := newScope(s)
+		for _, s := range stmt.Stmts {
+			retVals, ret, err := evalStmt(bs, s)
+			if err != nil || ret {
+				return retVals, ret, err
+			}
+		}
+		return nil, false, nil
+	}
+	panic(fmt.Sprintf("TODO evalStmt: %T: %s", stmt, stmt))
+}
+
+func evalLitOrVar(v interface{}) (interface{}, error) {
+	fmt.Printf("evalLitOrVar: %#+v\n", v)
+	switch v := v.(type) {
+	case *parser.BasicLiteral:
+		return v.Value, nil
+	case *parser.FuncLiteral:
+		return nil, fmt.Errorf("function not called")
+	case *Variable:
+		// TODO eww, but catched BasicLiteral in Variable
+		return evalLitOrVar(v.Value)
+	default:
+		return v, nil
+	}
+}
+
+func Eval(s *Scope, expr parser.Expr) (interface{}, error) {
+	v, err := eval(s, expr)
+	if err != nil {
+		return nil, err
+	}
+	if v, ok := v.([]interface{}); ok {
+		for i, iv := range v {
+			v[i], err = evalLitOrVar(iv)
+			if err != nil {
+				return nil, err
+			}
+		}
+		if len(v) == 1 {
+			return v[0], nil
+		}
+		return v, nil
+	}
+	return evalLitOrVar(v)
 }
 
 func binOp(op parser.Token, x, y interface{}) (interface{}, error) {
@@ -185,8 +325,32 @@ func binOp(op parser.Token, x, y interface{}) (interface{}, error) {
 			}
 		}
 	case parser.Less:
+		switch x := x.(type) {
+		case *big.Int:
+			switch y := y.(type) {
+			case *big.Int:
+				return x.Cmp(y) == -1, nil
+			}
+		case *big.Float:
+			switch y := y.(type) {
+			case *big.Float:
+				return x.Cmp(y) == -1, nil
+			}
+		}
 	case parser.Greater:
-		return nil, fmt.Errorf("unknown binop %s", op)
+		switch x := x.(type) {
+		case *big.Int:
+			switch y := y.(type) {
+			case *big.Int:
+				return x.Cmp(y) == 1, nil
+			}
+		case *big.Float:
+			switch y := y.(type) {
+			case *big.Float:
+				return x.Cmp(y) == 1, nil
+			}
+		}
 	}
-	return nil, fmt.Errorf("type mismatch Left: %T, Right: %T", x, y)
+	//return nil, fmt.Errorf("type mismatch Left: %T, Right: %T", x, y)
+	panic(fmt.Sprintf("type mismatch Left: %T, Right: %T", x, y))
 }
--- eval/eval_test.go
+++ eval/eval_test.go
@@ -11,13 +11,69 @@ import (
 	"numgrad.io/parser"
 )
 
-func TestTrivialEval(t *testing.T) {
-	s := &Scope{
-		Ident: map[string]interface{}{
-			"x": big.NewInt(4),
-			"y": big.NewInt(5),
-		},
+var exprTests = []struct {
+	expr string
+	want *big.Int
+}{
+	{"2+3*(x+y-2)", big.NewInt(23)},
+	{
+		`func() val {
+			if x > 2 {
+				return z+1
+			} else {
+				return z-1
+			}
+		}()`,
+		big.NewInt(8),
+	},
+	{
+		`func() val {
+				x := 9
+				x++
+				if x > 5 {
+					x = -x
+				}
+				return x
+			}()`,
+		big.NewInt(-10),
+	},
+}
+
+func TestExprs(t *testing.T) {
+	for _, test := range exprTests {
+		s := &Scope{
+			Ident: map[string]*Variable{
+				"x": &Variable{big.NewInt(4)},
+				"y": &Variable{big.NewInt(5)},
+			},
+			Parent: &Scope{
+				Ident: map[string]*Variable{
+					"x": &Variable{big.NewInt(1)}, // shadowed by child scope
+					"z": &Variable{big.NewInt(7)},
+				},
+			},
+		}
+
+		expr := mustParse(test.expr)
+		res, err := Eval(s, expr)
+		if err != nil {
+			t.Fatal(err)
+		}
+		got, ok := res.(*big.Int)
+		if !ok {
+			t.Fatalf("Eval(%s) want *big.Int, got: %s (%T)", expr, got, got)
+		}
+		if test.want.Cmp(got) != 0 {
+			t.Errorf("Eval(%s)=%s, want %s", expr, got, test.want)
+		}
 	}
+}
+
+func TestTrivialEval(t *testing.T) {
+	s := &Scope{Ident: map[string]*Variable{
+		"x": &Variable{big.NewInt(4)},
+		"y": &Variable{big.NewInt(5)},
+	}}
 	expr := mustParse("2+3*(x+y-2)")
 	res, err := Eval(s, expr)
 	if err != nil {
