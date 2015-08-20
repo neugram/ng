3a7891d62a3f1eeb21241d4ec393c76c249cd73e
David Crawshaw <david@zentus.com>
2015-08-20 18:03:31 -0400

    parser: scanner tests need to expect injected semicolon

--- parser/scanner_test.go
+++ parser/scanner_test.go
@@ -13,30 +13,31 @@ import (
 )
 
 type scannerTest struct {
-	input   string
-	token   token.Token
-	literal interface{}
+	input       string
+	token       token.Token
+	literal     interface{}
+	semiFollows bool
 }
 
 var scannerBothTests = []scannerTest{
-	{"foo", token.Ident, "foo"},
-	{"+", token.Add, nil},
-	{"*", token.Mul, nil},
-	{"bar", token.Ident, "bar"},
-	{"9", token.Int, big.NewInt(9)},
-	{"+=", token.AddAssign, nil},
-	{"++", token.Inc, nil},
-	{"break", token.Break, nil},
-	{"/* a block comment */", token.Comment, "/* a block comment */"},
-	{"(", token.LeftParen, nil},
-	{"4.62e12", token.Float, big.NewFloat(4.62e12)},
-	{")", token.RightParen, nil},
-	{"//final", token.Comment, "//final"},
+	{"foo", token.Ident, "foo", true},
+	{"+", token.Add, nil, false},
+	{"*", token.Mul, nil, false},
+	{"bar", token.Ident, "bar", true},
+	{"9", token.Int, big.NewInt(9), true},
+	{"+=", token.AddAssign, nil, false},
+	{"++", token.Inc, nil, true},
+	{"break", token.Break, nil, true},
+	{"/* a block comment */", token.Comment, "/* a block comment */", false},
+	{"(", token.LeftParen, nil, false},
+	{"4.62e12", token.Float, big.NewFloat(4.62e12), true},
+	{")", token.RightParen, nil, true},
+	{"//final", token.Comment, "//final", false},
 }
 
 var scannerSepTests = append([]scannerTest{
-	{"// a line comment", token.Comment, "// a line comment"},
-	{"      // spaced ", token.Comment, "// spaced "},
+	{"// a line comment", token.Comment, "// a line comment", false},
+	{"      // spaced ", token.Comment, "// spaced ", false},
 }, scannerBothTests...)
 
 var scannerJoinTests = append([]scannerTest{}, scannerBothTests...)
@@ -50,9 +51,21 @@ func TestScannerSep(t *testing.T) {
 		}
 		if s.Token != test.token {
 			t.Errorf("%q: got %s, want %s", test.input, s.Token, test.token)
+			continue
 		}
 		if !equalLiteral(s.Literal, test.literal) {
 			t.Errorf("%q literal: got %s, want %s", test.input, s.Literal, test.literal)
+			continue
+		}
+		if test.semiFollows {
+			if err := s.Next(); err != nil {
+				t.Errorf("%q: error expecting ';': %v", test.input, err)
+				continue
+			}
+			if s.Token != token.Semicolon {
+				t.Errorf("%q: expected ';', got: %v", test.input, s.Token)
+				continue
+			}
 		}
 		if err := s.Next(); err != io.EOF {
 			t.Errorf("%q: expected EOF, got: %v", test.input, err)
