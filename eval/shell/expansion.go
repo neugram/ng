98ef20a63a4f52af169770628c21649f430bc881
David Crawshaw <david@zentus.com>
2016-01-05 20:35:11 -0500

    shell: make brace expansion sensitive to quoting

--- eval/shell/expansion.go
+++ eval/shell/expansion.go
@@ -3,8 +3,6 @@
 
 package shell
 
-import "strings"
-
 func expansion(argv1 []string, params Params) ([]string, error) {
 	var err error
 	var argv2 []string
@@ -32,18 +30,18 @@ var expanders = []func([]string, string, Params) ([]string, error){
 // brace expansion (for example: "c{d,e}" becomes "cd ce")
 func braceExpand(src []string, arg string, _ Params) (res []string, err error) {
 	res = src
-	i1 := strings.IndexRune(arg, '{')
+	i1 := indexUnquoted(arg, '{')
 	if i1 == -1 {
 		return append(res, arg), nil
 	}
-	i2 := strings.IndexRune(arg[i1:], '}')
+	i2 := indexUnquoted(arg[i1:], '}')
 	if i2 == -1 {
 		return append(res, arg), nil
 	} else {
 		prefix, suffix := arg[:i1], arg[i1+i2+1:]
 		arg = arg[i1+1 : i1+i2]
 		for len(arg) > 0 {
-			c := strings.IndexRune(arg, ',')
+			c := indexUnquoted(arg, ',')
 			if c == -1 {
 				res, _ = braceExpand(res, prefix+arg+suffix, nil)
 				break
@@ -71,4 +69,33 @@ func paramExpand(src []string, arg string, params Params) (res []string, err err
 func pathsExpand(src []string, arg string, params Params) (res []string, err error) {
 	// TODO
 	return append(src, arg), nil
+}
+
+// indexUnquoted returns the index of the first unquoted Unicode code
+// point r, or -1. A code point r is quoted if it is directly preceded
+// by a '\' or enclosed in "" or ''.
+func indexUnquoted(s string, r rune) int {
+	prevSlash := false
+	inBlock := rune(-1)
+	for i, v := range s {
+		if inBlock != -1 {
+			if v == inBlock {
+				inBlock = -1
+			}
+			continue
+		}
+
+		if !prevSlash {
+			switch v {
+			case r:
+				return i
+			case '\'', '"':
+				inBlock = v
+			}
+		}
+
+		prevSlash = v == '\\'
+	}
+
+	return -1
 }
